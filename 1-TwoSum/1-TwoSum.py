874de43f51539636a2aa4a5e13eadf09e1d6c39c
Narayan Sapkota <34578987+NaranSapkota@users.noreply.github.com>
2025-11-13 20:10:05 -0500

    Delete 1-TwoSum directory

--- 1-TwoSum/1-TwoSum.py
+++
@@ -1,9 +0,0 @@
-# Last updated: 11/11/2025, 7:48:09 PM
-class Solution(object):
-    def twoSum(self, nums, target):
-        for i in range(0, len(nums) - 1):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return []
-        
