ee32ce848e14f91eb53e5ae46cdcd3c92371e261
Narayan Sapkota <34578987+NaranSapkota@users.noreply.github.com>
2025-11-16 19:37:04 -0500

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.py
@@ -0,0 +1,24 @@
+# Last updated: 11/16/2025, 7:37:05 PM
+class Solution:
+    def reverse(self, x: int) -> int:
+        
+        min= -2147483648
+        max=2147483647
+        
+        res=0
+        while x:
+            digit=int(math.fmod(x,10))
+            x=int(x/10)
+            
+            if (res> max // 10 or (res ==max //10 and digit >=max%10)):
+                return 0
+            
+            if (res< min // 10 or (res ==min //10 and digit <=min%10)):
+                return 0
+            
+            res=(res*10) +digit
+            
+        return res
+            
+                 
+                 
